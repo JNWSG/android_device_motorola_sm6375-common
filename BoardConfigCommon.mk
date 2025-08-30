12adb6fd1f0ebf72a065efa846cd6d7b25a14b2f
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-30 23:45:15 +0800

    sm6375-common: Cleanup more flags
    These are gone after bp conversion for display and media
    
    Change-Id: I1169adf12d717417f5f328ebbcc01ecc9b911889

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -112,9 +112,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/manifest.xml
 
-# Media
-TARGET_USES_ION := true
-
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
